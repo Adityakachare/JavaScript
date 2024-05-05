7f1963b28af5997c3470bcc736065db8435b5847
Adityakachare <78196873+Adityakachare@users.noreply.github.com>
2024-05-05 12:43:51 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,58 @@
+//JavaScript arrays are resizable
+//Can have mixed datatype(String, number, objects...etc)
+//Arrays are zero-indexed
+
+//JavaScript array-copy-operations create SHALLOW COPIES
+
+/* 
+SHALLOW COPIES: A shallow copy of an object is a copy whose properties share the same references as those of the source object from which the copy was made.
+
+DEEP COPIES: A deep copy of an object is a copy whose properties do not share the same reference as those of the source object from which the copy was made.
+
+Deep copies create entirely new objects, ensuring that changes to the copy do not affect the original object, while shallow copies retain references to the original object's properties, meaning changes to nested objects in the copy will affect the original.
+*/
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["IronMan", "SpiderMan", "CaptainAmerica"]
+const myArr = new Array(1, 2, 3)
+console.log(myArray[1]);
+
+//Array Methods
+// myArray.push(6)
+// myArray.push(7)
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
+
+myArray.unshift(9)
+console.log(myArray);
+/*[
+    9, 0, 1, 2,
+    3, 4, 5
+]*/
+
+myArray.shift()
+console.log(myArray);
+
+console.log(myArray.includes(8));
+console.log(myArray.indexOf(3));
+
+const newArray1 = myArray.join();
+console.log(myArray);
+console.log(typeof newArray1); //join will make the array in string
+
+//SLICE and SPLICE
+console.log("A ", myArray);
+const myArray1 = myArray.slice(1, 3)
+console.log(myArray1);
+console.log("B ", myArray);
+
+const myArray2 = myArray.splice(1, 3)
+console.log("C ", myArray);
+console.log(myArray2);
+/*
+Slice method will return the subset of the array according to the parameters given in the parenthesis. It won't consider the element at the 2nd parameter. It is exclusive
+On the other hand the Splice method is inclusive and will consider the element at the 2nd parameter.
+
+BUT the major difference between slice and splice is that SPLICE manipulates the original array where as slice dosen't
+*/
+
---
+++ 02_basics/02_array.js
@@ -0,0 +1,80 @@
+const marvelHeros = ["Thor", "IronMan", "SpiderMan"]
+const dcHeros = ["Batman", "Superman", "Flash"]
+
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros); 
+/*
+[ 'Thor', 'IronMan', 'SpiderMan', [ 'Batman', 'Superman', 'Flash' ] ] - array inside array 
+    1         2          3                          4
+*/
+// console.log(marvelHeros[3]); //[ 'Batman', 'Superman', 'Flash' ]
+// console.log(marvelHeros[3][1]); // Superman
+
+const allHeros = marvelHeros.concat(dcHeros)
+console.log(allHeros); //[ 'Thor', 'IronMan', 'SpiderMan', 'Batman', 'Superman', 'Flash' ]
+
+/*
+PUSH method adds the elements in the existing array itself
+whereas
+CONCAT method add the elements and returns a new array with the added elements
+*/
+
+const allNewHeros = [...marvelHeros, ...dcHeros] 
+console.log(allNewHeros); 
+console.log(typeof allNewHeros); //object
+/*
+SPREAD OPERATOR(...)
+the array elements become individual elements therefore when the two arrays are passed along with a comma the elements of each array are printed which is similar to the concat operator.
+
+The advantage of SPREAD operator over concat and push is that we can pass more than two arrays in it which will be joined together, following is the example:
+*/
+const IndianHeros = ["Shaktiman", "Naagraj", "Minnal Murali"]
+const allNewHeros1 = [...marvelHeros, ...dcHeros, ...IndianHeros]
+console.log(allNewHeros1);
+/**
+[
+  'Thor',
+  'IronMan',
+  'SpiderMan',
+  'Batman',
+  'Superman',
+  'Flash',
+  'Shaktiman',
+  'Naagraj',
+  'Minnal Murali'
+]
+*/
+
+
+/*
+The flat() method, when called with Infinity, recursively flattens nested arrays to the specified depth, or until all nested arrays are flattened into a single-level array.
+
+We need to pass the depth till which we need to flatten the array 
+In realAnotherArray1 we have flattened the array till 1 depth therefore in the output the array in array is not resolved/flattened
+If we don't know the depth till which we need to flatten it we can simply pass "Infinity"
+*/
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const realAnotherArray = anotherArray.flat(Infinity)
+const realAnotherArray1 = anotherArray.flat(1)
+console.log(realAnotherArray);
+/*
+[
+  1, 2, 3, 4, 5,
+  6, 7, 6, 7, 4,
+  5
+]
+*/
+console.log(realAnotherArray1); //[ 1, 2, 3, 4, 5, 6, 7, 6, 7, [ 4, 5 ] ]
+
+console.log(Array.isArray("Aditya")) //False
+console.log(Array.from("Aditya")) //[ 'A', 'd', 'i', 't', 'y', 'a' ]
+console.log(Array.from({name:"Aditya", age: 21, DOB: "27th Feb, 2003"})) // will return an empty array as we need to specify on what basis the array is to be made, if the function is not able to understand it, it will always return an empty string
+const aditya = {name:"Aditya", age: 21, DOB: "27th Feb, 2003"} 
+const resultArray = Array.from(Object.keys(aditya));
+console.log(resultArray); //['name', 'age', 'DOB']
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
